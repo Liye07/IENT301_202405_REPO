12b387568a8dc8dd9942254cbed7a25ed4ea12bf
Liyema Booi <s225025426@mandela.ac.za>
2024-05-22 13:48:30 +0200

    Add project files.

---
+++ 225008580_MB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makha_Zanele_Liyema
+{
+    internal class _225008580_MB
+    {
+        string name = "Makhapongo";
+    }
+}
---
+++ 225025426_LB.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Makha_Zanele_Liyema
+{
+    internal class _225025426_LB
+    {
+        string name = "Liyema";
+    }
+}
